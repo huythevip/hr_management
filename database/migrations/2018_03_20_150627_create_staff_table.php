791e8959e1b8d3244a2b92e4b3c027308e50fff0
huythevip <huy.tq2106@gmail.com>
2018-03-20 22:43:52 +0700

    Created model and migrations. Also set relationships.

---
+++ database/migrations/2018_03_20_150627_create_staff_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('department');
+            $table->string('position');
+            $table->string('skill');
+            $table->string('year_exp');
+            $table->string('year_join');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
