7758fe3c557146679d481cf8c0c7a44010726e8d
An Vo <an.vo@hotelquickly.com>
2018-03-21 22:51:36 +0700

    demo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,55 @@
 <!-- Content -->
 
 @section('content')
+    <div class="content_test">
+
+    </div>
+
+@endsection
+
+@section('scripts')
+    <script>
+        $(document).ready(function($) {
+            $.ajax({
+                url: "api/api_test",
+                data: {
+                    name: 'abc',
+                    id: 1
+                },
+                success: function(result){
+                    console.log(result);
+
+                    data = JSON.parse(result);
+
+                    error = data.error;
+
+                    if(error == true) {
+                        alert(data.message);
+                    }else {
+                        string = JSON.stringify(data.data);
+                        $('.content_test').text(string);
+                    }
+                }});
+
+            $.ajax({
+                url: "api/departments",
+                method: "POST",
+                data: {
+                    title: 'title1',
+                    department_id: 5
+                },
+                success: function(result) {
+                    console.log(result);
+                }
 
 
+
+
+            })
+
+        })
+
+
+
+    </script>
 @endsection
