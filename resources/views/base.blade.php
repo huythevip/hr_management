55502ade5e54b887185660b952452cd0e69860a3
An Vo <an.vo@hotelquickly.com>
2018-03-24 00:08:30 +0700

    Add some css

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -1,65 +1,81 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <style>
-            * {
-                margin: 0px;
-                padding: 0px;
-                box-sizing: border-box;
-                -moz-box-sizing: border-box;
-                -webkit-box-sizing: border-box;
-            }
+<head>
+    <meta charset="utf-8">
+    <style>
+        /** {*/
+        /*margin: 0px;*/
+        /*padding: 0px;*/
+        /*box-sizing: border-box;*/
+        /*-moz-box-sizing: border-box;*/
+        /*-webkit-box-sizing: border-box;*/
+        /*}*/
 
+        /*#logo {*/
+        /*text-decoration: none;*/
+        /*color: white;*/
+        /*font-size: 25px;*/
+        /*margin-left: 150px;*/
+        /*}*/
 
-            #logo {
-                text-decoration: none;
-                color: white;
-                font-size: 25px;
-                margin-left: 150px;
-            }
+        /*#search-bar {*/
+        /*float: right;*/
+        /*margin-right: 30px;*/
+        /*}*/
 
-            #search-bar {
-                float: right;
-                margin-right: 30px;
-            }
+        /*#search-bar input {*/
+        /*height: 40px;*/
+        /*}*/
 
-            #search-bar input {
-                height: 40px;
-            }
+        /*#search-bar button {*/
+        /*height: 40px;*/
+        /*}*/
 
-            #search-bar button {
-                height: 40px;
-            }
+        /*}*/
 
-            }
+        /*input {*/
+        /*width: 200px;*/
+        /*height: 40px;*/
+        /*padding: 10px 20px;*/
+        /*}*/
 
-            input {
-                width: 200px;
-                height: 40px;
-                padding: 10px 20px;
-            }
+        @yield('custom_css')
 
-            @yield('custom_css')
+    </style>
 
-        </style>
-        <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+</head>
+<body>
+<div class="container-fluid" style="background-color: #9542f4; ">
+    <div class="row">
+        <form class="" role="search" id="search-bar" action="#" method="POST">
+            <div class="col-xs-6">
+                <h1 href="$" id="logo">Hr Management</h1>
+            </div>
+            <div class="col-xs-6 pull-right">
+                <div class="row">
+                    <div class="col-xs-6 col-md-3">
+                        <div class="form-group" style="margin-top: 25px;">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                    </div>
+                    <div class="col-xs-6">
+                        <button type="submit" class="btn btn-default" style="margin-top: 25px;">Search</button>
+                    </div>
+                </div>
 
-    </head>
-    <body>
-        <div class="container-fluid" style="background-color: #9542f4; height: 50px; line-height: 50px;">
-            <a href="$" id="logo">Hr Management</a>
-                <form  id="search-bar" action="#" method="POST">
-                <input type="text" placeholder="Search">
-                <button type="submit">Search</button>
-                </form>
-        </div> <!-- Header Container -->
+            </div>
 
-        @yield('content')
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        @yield('scripts')
-    </body>
+        </form>
+    </div>
+</div> <!-- Header Container -->
+
+@yield('content')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+@yield('scripts')
+</body>
 
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,105 +3,119 @@
 
 <!-- CSS -->
 @section('custom_css')
-.content_test {
-    width: 1000px;
-    height: 500px;
-    margin: 0 auto;
-    margin-top: 50px;
-}
+    {{--.content_test {--}}
+    {{--width: 1000px;--}}
+    {{--height: 500px;--}}
+    {{--margin: 0 auto;--}}
+    {{--margin-top: 50px;--}}
+    {{--}--}}
 
-.information-panel {
-    height: 50px;
-    line-height: 50px;
-}
+    {{--.information-panel {--}}
+    {{--height: 50px;--}}
+    {{--line-height: 50px;--}}
+    {{--}--}}
 
-.information-panel h1 {
-    float: left;
-    display: inline-block;
-    margin-left: 50px;
-}
+    {{--.information-panel h1 {--}}
+    {{--float: left;--}}
+    {{--display: inline-block;--}}
+    {{--margin-left: 50px;--}}
+    {{--}--}}
 
-.information-panel select {
-    margin-left: 50px;
-    height: 30px;
-}
+    {{--.information-panel select {--}}
+    {{--margin-left: 50px;--}}
+    {{--height: 30px;--}}
+    {{--}--}}
 
-.department-staff {
-    clear: left;
-    width: 100%; 
-    height: 90%;
-}
-
+    {{--.department-staff {--}}
+    {{--clear: left;--}}
+    {{--width: 100%; --}}
+    {{--height: 90%;--}}
+    {{--}--}}
+    .abc {
+    margin-bottom: 20px;
+    }
+    #information-panel {
+    margin-top: 20px;
+    }
+    #my_table {
+    width: 100%;
+    }
+    .xuong_20_px {
+        margin-bottom: 20px;
+    }
 @endsection
 
 <!-- Content -->
 
 @section('content')
-    <div class="content_test">
-        <div class="information-panel">
-        <h1>Department:</h1>
-        <select id="selected_dept">
-            <option>Select</option>
-            @foreach($departments as $department)
-                <option>{{$department->full_name}}</option>
-            @endforeach
-        </select>
-        </div> <!-- Information Panel -->
-        <div class="department-staff">
-        <table>
-            <thead>
-                <tr id="staff_info">
-                    
-                </tr>
-            </thead>
-            <tbody id="staff_detail">
-                
-                    
-                
-            </tbody>
-        </table>
-        </div> <!-- Department-staff -->
-        
-    </div> <!-- Content_test -->
+    <div class="container">
+        <div class="row">
+            <div class="abc">
+                <form id="information-panel" class=" form-inline">
+                    <label for="sel1">Department:</label>
+                    <select class="form-control" id="selected_dept">
+                        <option>Please select</option>
+                        @foreach($departments as $department)
+                            <option>{{$department->full_name}}</option>
+                        @endforeach
+                    </select>
+                </form>
+            </div>
+        </div>
+
+
+        <div class="row xuong_20_px">
+            <a href="#" class="btn btn-primary">Create</a>
+        </div>
+
+        <div class="row">
+            <div class="department-staff">
+                <table id="my_table" class="table">
+                    <thead>
+                    <tr id="staff_info" class="info">
+                        <td width="20%">Full Name</td>
+                        <td width="20%">Position</td>
+                        <td width="40%">Skill</td>
+                        <td width="10%">Experience</td>
+                        <td width="10%">Year Joined</td>
+                    </tr>
+                    </thead>
+                    <tbody id="staff_detail">
+
+
+                    </tbody>
+                </table>
+            </div> <!-- Department-staff -->
+        </div>
+
+    </div>
 
 @endsection
 
 @section('scripts')
     <script>
         $(document).ready(function () {
-            $.ajax({
-                url:'api/department_column',
-                data: {},
-                success: function (dept_col) {
-                    data = JSON.parse(dept_col);
-                    cols = data.columns;
-                    html = '';
-                    cols.forEach(function(col) {
-                        html += '<td>'+col+'</td>';
+            $('#selected_dept').change(function () {
+                $('#staff_detail').html('');
+                $.ajax({
+                    url: 'api/staff_list',
+                    method: 'GET',
+                    data: {'department': $('#selected_dept').val()},
+                    dataType: 'JSON',
+                    success: function (staffs) {
+                        staffs.forEach(function (staff) {
+                            $('#staff_detail').append('<tr>\
+                            <td>' + staff.full_name + '</td>\
+                            <td>' + staff.position + '</td>\
+                            <td>' + staff.skill + '</td>\
+                            <td>' + staff.year_exp + '</td>\
+                            <td>' + staff.year_join + '</td></tr>')
+                        });
+                    },
                 });
-                    $('#staff_info').html(html);
-                },
-            });
-            });
-        $('#selected_dept').change(function() {
-            $('#staff_detail').html('');
-            $.ajax({
-                url:'api/staff_list',
-                method: 'GET',
-                data: {'department': $('#selected_dept').val()},
-                dataType: 'JSON',
-                success: function(staffs) {
-                    staffs.forEach(function(staff) {
-                        $('#staff_detail').append('<tr>\
-                            <td>'+staff.full_name+'</td>\
-                            <td>'+staff.position+'</td>\
-                            <td>'+staff.skill+'</td>\
-                            <td>'+staff.year_exp+'</td>\
-                            <td>'+staff.year_join+'</td></tr>')
-                    });
-                },
-            });
-        })
+            })
+        });
+
     </script>
 
 @endsection
